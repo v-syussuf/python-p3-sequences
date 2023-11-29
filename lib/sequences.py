86e9ca40794ffff1e52dac65e461c26438de28cf
Yussuf <yussuf.saumu@gmail.com>
2023-11-29 09:15:34 +0300

    Assignment Complete

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci_sequence = [0, 1]
+
+    while len(fibonacci_sequence) < length:
+        next_number = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+        fibonacci_sequence.append(next_number)
+
+    print(fibonacci_sequence)
+
+print_fibonacci(7)
